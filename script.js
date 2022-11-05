461478ef9c3e720dff450154ad7d557577225e8e
KricsDavid <71211940+KricsDavid@users.noreply.github.com>
2022-11-05 18:25:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function plusz(){
+    elso = parseInt(document.getElementById("elso").value);
+    masodik = parseInt(document.getElementById("masodik").value);
+    document.getElementById("Eredmeny").innerHTML = elso + masodik;
+}
+
+function minusz(){
+    elso = parseInt(document.getElementById("elso").value);
+    masodik = parseInt(document.getElementById("masodik").value);
+    document.getElementById("Eredmeny").innerHTML = elso - masodik;
+}
+
+function szor(){
+    elso = parseInt(document.getElementById("elso").value);
+    masodik = parseInt(document.getElementById("masodik").value);
+    document.getElementById("Eredmeny").innerHTML = elso * masodik;
+}
+
+function oszt(){
+    elso = parseInt(document.getElementById("elso").value);
+    masodik = parseInt(document.getElementById("masodik").value);
+    document.getElementById("Eredmeny").innerHTML = elso / masodik;
+}
